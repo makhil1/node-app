d231a9b5e391012e2d7909f753fc16f07c4b1eab
makhil1 <makhil@hotmail.com>
2020-10-20 22:51:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">welcome to new Akhil Node App - CI/CD Codepipeline-1234!!</h1> \n');
+  res.send('<h1 style="color:red;">welcome to Docker world - new CI/CD Codepipeline!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
